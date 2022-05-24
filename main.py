626288d051738f936dc5a78a36d4388c34c12fdb
Gabriel <gabriel@air-gabriel.mynet>
2022-05-24 21:34:28 +0200

    proba2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from math import pi
 #a, b, c = [int(a) for a in input().split()]
 #a, b, c = input()
+s = float
+pole = float
 inputs = []
 for i in range(3):
     inputs.append(input())
@@ -11,10 +13,12 @@ if b == 0 and c == 0:
     def policz_kolo(a):
         return pi * a * a
     pole = '%0.2f' %policz_kolo(a)
+    print(pole)
 elif c == 0:
     def policz_prostokata(a,b):
         return a * b
     pole = '%0.2f' %policz_prostokata(a,b)
+    print(pole)
 elif a != 0 and b != 0 and c != 0:
     s = (a + b + c) / 2
     def policz_trojkata(a,b,c):
@@ -26,6 +30,7 @@ elif a != 0 and b != 0 and c != 0:
           #  return (1/2)*a*b 
         return (s*(s-a)*(s-b)*(s-c)) ** 0.5 
     pole = '%0.2f' %policz_trojkata(a,b,c)
+    print(pole)
 else:
     print("Błąd: można podać maksymalnie 3 liczby")
-print(pole)
+#print(pole)
