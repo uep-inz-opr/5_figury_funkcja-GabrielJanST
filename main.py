c11d1f0eb1ed5c98950b89c61303d5bde3086853
Gabriel <gabriel@air-gabriel.mynet>
2022-05-24 21:56:41 +0200

    proba 4

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 from math import pi
 lst = list(map(int, input().split()))
-#a, b, c = [int(a) for a in input().split()]
-#a, b, c = input()
-s = float
-pole = float
-inputs = []
-#for i in range(3):
-#    inputs.append(input())
-#a = float(lst[0])
-#b = float(lst[1])
-#c = float(lst[2])
-#if b == 0 and c == 0:
+#s = float
+#pole = float
 if len(lst) == 1:
     a = float(lst[0])
     def policz_kolo(a):
@@ -32,15 +23,8 @@ elif len(lst) == 3:
     c = float(lst[2])
     s = ((a + b + c)/2)
     def policz_trojkata(a,b,c):
-        #if a > b and a > c:
-         #   return (1/2)*b*c
-        #elif b > a and b > c:
-         #   return (1/2)*a*c
-        #elif c > a and c > b:
-          #  return (1/2)*a*b 
         return (s*(s-a)*(s-b)*(s-c)) ** 0.5 
     pole = '%0.2f' %policz_trojkata(a,b,c)
     print(pole)
 else:
     print("Błąd: można podać maksymalnie 3 liczby")
-#print(pole)
