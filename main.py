8dea54eb64daaf2a5d25c155e056ff7edff8b52f
Gabriel <gabriel@air-gabriel.mynet>
2022-05-24 23:27:36 +0200

    proba 6

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from math import pi
-lst = list(map(float, input().split()))
+lst = []
+wynik = 0
+liczba_figur = int(input())
+for i in range(liczba_figur):
+    lst = list(map(float, input().split()))
 #s = float
 #pole = float
 if len(lst) == 1:
@@ -7,7 +11,12 @@ if len(lst) == 1:
     def policz_kolo(a):
         return pi * a * a
     pole = '%0.2f' %policz_kolo(a)
-    print(pole)
+    if wynik == 0:
+        wynik = pole
+    else:
+        #wynik = wynik + pole
+        wynik = sum(wynik,pole)
+    #wynik.append(pole)
 #elif c == 0:
 elif len(lst) == 2:
     a = float(lst[0])
@@ -15,8 +24,12 @@ elif len(lst) == 2:
     def policz_prostokata(a,b):
         return a * b
     pole = '%0.2f' %policz_prostokata(a,b)
-    print(pole)
-#elif a != 0 and b != 0 and c != 0:
+    if wynik == 0:
+        wynik = pole
+    else:
+        wynik = sum(wynik,pole)
+    #wynik.append((pole).strip())
+    #print(pole)
 elif len(lst) == 3:
     a = float(lst[0])
     b = float(lst[1])
@@ -25,6 +38,12 @@ elif len(lst) == 3:
     def policz_trojkata(a,b,c):
         return (s*(s-a)*(s-b)*(s-c)) ** 0.5 
     pole = '%0.2f' %policz_trojkata(a,b,c)
-    print(pole)
+    if wynik == 0:
+        wynik = pole
+    else:
+        wynik = sum(wynik,pole)
+    #wynik.append((pole).strip())
+    #print(pole)
 else:
     print("Błąd: można podać maksymalnie 3 liczby")
+print ((wynik))
